73790b97919935099200f2d391d0332801bd11a2
Pranay2006 <pranaynagrale358@gmail.com>
2022-10-12 17:28:51 +0530

    Test

---
+++ src/com/online/restaurent/OrderMenuItem.java
@@ -0,0 +1,41 @@
+package com.online.restaurent;
+
+public class OrderMenuItem {
+    private String menuItem;
+    private int quantity;
+    private double price;
+
+    public String getMenuItem() {
+        return menuItem;
+    }
+
+    public void setMenuItem(String menuItem) {
+        this.menuItem = menuItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isVeg() {
+        return isVeg;
+    }
+
+    public void setVeg(boolean veg) {
+        isVeg = veg;
+    }
+
+    private boolean isVeg;
+}
